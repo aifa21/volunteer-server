280a1b3771b4d8d6e92472468e3b8c2ad4e34fd1
aifa21 <37891597+aifa21@users.noreply.github.com>
2020-10-05 23:21:58 +0600

    root api created

--- index.js
+++ index.js
@@ -13,6 +13,9 @@ app.use(cors());
 
 const port = 5000
 
+app.get('/',(req,res)=>{
+  res.send("WELCOME TO VOLUNTEER NETWORK")
+})
 
 
 const client = new MongoClient(uri, { useNewUrlParser: true,useUnifiedTopology:true });
